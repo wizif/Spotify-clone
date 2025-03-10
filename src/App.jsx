f3c5157d675a0cf9ced221b9a41603703cd1550d
eezlogicxArvind <the1warriorx@gmail.com>
2025-03-10 22:24:34 +0530

    songitemr

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Sidebar from './components/Sidebar'
+import Player from './components/Player'
+import Display from './components/Display'
 
 const App = () => {
   return (
     <div className='h-screen bg-black'>
-      <div className='h-[90%] flex'></div>
+      <div className='h-[90%] flex'>
+
    <Sidebar/>
+   <Display/>
+      </div>
+      <Player/>
     </div>
   )
 }
---
+++ src/components/DisplayHome.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Navbar from "./Navbar";
+import { albumsData } from "../assets/frontend-assets/assets";
+import AlbumItem from "./AlbumItem";
+import { songsData } from "../assets/frontend-assets/assets";
+
+const DisplayHome = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl ">Feature charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl ">Todays biggest hits</h1>
+        <div className="flex overflow-auto">
+         
+         
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DisplayHome;
