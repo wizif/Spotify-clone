a055b305a8d2a5bae80900298851e82714851b08
eezlogicxArvind <the1warriorx@gmail.com>
2025-03-12 22:30:48 +0530

    2

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import PlayerContextProvider from "./context/PlayerContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
-</BrowserRouter>
-  </StrictMode>,
-)
+    <PlayerContextProvider>
+
+      <App />
+    </PlayerContextProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
